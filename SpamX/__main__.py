ab9fc7240e40e7901f966324c86fb27fa41fd453
Itzvenomo <118875769+Itzvenomo@users.noreply.github.com>
2023-02-12 15:48:23 +0530

    Add files via upload

---
+++ SpamX/__main__.py
@@ -0,0 +1,98 @@
+# Venom - Telegram Projects
+# (c) 2022 - 2024 Venom
+# Don't Kang Bitch -!
+
+
+
+import asyncio
+from pyrogram import idle
+from . import (VenomX, VenomX2, VenomX3, VenomX4, VenomX5, 
+                VenomX6, VenomX7, VenomX8, VenomX9, VenomX10, 
+                VenomX11, VenomX12, VenomX13, VenomX14, VenomX15, 
+                VenomX16, VenomX17, VenomX18, VenomX19, VenomX20, hl)
+
+if VenomX:
+    VenomX.Venomt()
+    VenomX.join_chat("Its_Venom_family")
+    VenomX.join_chat("Its_Venom_family")
+if VenomX2:
+    VenomX2.Venomt()
+    VenomX2.join_chat("Its_Venom_family")
+    VenomX2.join_chat("Its_Venom_family")
+if VenomX3:
+    VenomX3.Venomt()
+    VenomX3.join_chat("Its_Venom_family")
+    VenomX3.join_chat("Its_Venom_family")
+if VenomX4:
+    VenomX4.Venomt()
+    VenomX4.join_chat("Its_Venom_family")
+    VenomX4.join_chat("Its_Venom_family")
+if VenomX5:
+    VenomX5.Venomt()
+    VenomX5.join_chat("Its_Venom_family")
+    VenomX5.join_chat("Its_Venom_family")
+if VenomX6:
+    VenomX6.Venomt()
+    VenomX6.join_chat("Its_Venom_family")
+    VenomX6.join_chat("Its_Venom_family")
+if VenomX7:
+    VenomX7.Venomt()
+    VenomX7.join_chat("Its_Venom_family")
+    VenomX7.join_chat("Its_Venom_family")
+if VenomX8:
+    VenomX8.Venomt()
+    VenomX8.join_chat("Its_Venom_family")
+    VenomX8.join_chat("Its_Venom_family")
+if VenomX9:
+    VenomX9.Venomt()
+    VenomX9.join_chat("Its_Venom_family")
+    VenomX9.join_chat("Its_Venom_family")
+if VenomX10:
+    VenomX10.Venomt()
+    VenomX10.join_chat("Its_Venom_family")
+    VenomX10.join_chat("Its_Venom_family")
+if VenomX11:
+    VenomX11.Venomt()
+    VenomX11.join_chat("Its_Venom_family")
+    VenomX11.join_chat("Its_Venom_family")
+if VenomX12:
+    VenomX12.Venomt()
+    VenomX12.join_chat("Its_Venom_family")
+    VenomX12.join_chat("Its_Venom_family")
+if VenomX13:
+    VenomX13.Venomt()
+    VenomX13.join_chat("Its_Venom_family")
+    VenomX13.join_chat("Its_Venom_family")
+if VenomX14:
+    VenomX14.Venomt()
+    VenomX14.join_chat("Its_Venom_family")
+    VenomX14.join_chat("Its_Venom_family")
+if VenomX15:
+    VenomX15.Venomt()
+    VenomX15.join_chat("Its_Venom_family")
+    VenomX15.join_chat("Its_Venom_family")
+if VenomX16:
+    VenomX16.Venomt()
+    VenomX16.join_chat("Its_Venom_family")
+    VenomX16.join_chat("Its_Venom_family")
+if VenomX17:
+    VenomX17.Venomt()
+    VenomX17.join_chat("Its_Venom_family")
+    VenomX17.join_chat("Its_Venom_family")
+if VenomX18:
+    VenomX18.Venomt()
+    VenomX18.join_chat("Its_Venom_family")
+    VenomX18.join_chat("Its_Venom_family")
+if VenomX19:
+    VenomX19.Venomt()
+    VenomX19.join_chat("Its_Venom_family")
+    VenomX19.join_chat("Its_Venom_family")
+if VenomX20:
+    VenomX20.Venomt()
+    VenomX20.join_chat("Its_Venom_family")
+    VenomX20.join_chat("Its_Venom_family")
+
+print("Your Spam Bots Successfully Deployed ✅")
+print("Visit -! @Its_Venom_family")
+
+idle()
