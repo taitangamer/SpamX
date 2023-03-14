0956231d131bf710726d74ce5d51a1f5ed22aef8
Itzvenomo <118875769+Itzvenomo@users.noreply.github.com>
2023-03-14 13:48:26 +0530

    Add files via upload

--- SpamX/__init__.py
+++ SpamX/__init__.py
@@ -1,196 +1,89 @@
-# Venom - Telegram Projects
-# (c) 2022 - 2024 Venom
-# Don't Kang Bitch -! 
-
-
-
-import asyncio
-import os
-import sys
+from pyrogram import Client
+from config import API_ID, API_HASH, SUDO_USERS, OWNER_ID, BOT_TOKEN, STRING_SESSION1, STRING_SESSION2, STRING_SESSION3, STRING_SESSION4, STRING_SESSION5, STRING_SESSION6, STRING_SESSION7, STRING_SESSION8, STRING_SESSION9, STRING_SESSION10
+from datetime import datetime
 import time
+from aiohttp import ClientSession
 
-from dotenv import load_dotenv
-from pyrogram import Client, filters
-
-ULOG = [5956803759 , -1001897175342]
-
-if os.path.exists(".env"):
-    load_dotenv(".env")
-    
-__version__ = "v0.1"
-
-# -------------CONFIGS--------------------
-API_ID = int(os.getenv("API_ID", ""))
-API_HASH = os.getenv("API_HASH", "")
-ALIVE_PIC = os.getenv("ALIVE_PIC", "")
-ALIVE_MSG = os.getenv("ALIVE_MSG", "")
-PING_MSG = os.getenv("PING_MSG", "")
-GIT_TOKEN = os.getenv("GIT_TOKEN", "")
-REPO_URL = os.getenv("REPO_URL", "") 
-BRANCH = os.getenv("BRANCH", "")
-SESSION = os.getenv("SESSION", None)
-SESSION2 = os.getenv("SESSION2", None)
-SESSION3 = os.getenv("SESSION3", None)
-SESSION4 = os.getenv("SESSION4", None)
-SESSION5 = os.getenv("SESSION5", None)
-SESSION6 = os.getenv("SESSION6", None)
-SESSION7 = os.getenv("SESSION7", None)
-SESSION8 = os.getenv("SESSION8", None)
-SESSION9 = os.getenv("SESSION9", None)
-SESSION10 = os.getenv("SESSION10", None)
-SESSION11 = os.getenv("SESSION11", None)
-SESSION12 = os.getenv("SESSION12", None)
-SESSION13 = os.getenv("SESSION13", None)
-SESSION14 = os.getenv("SESSION14", None)
-SESSION15 = os.getenv("SESSION15", None)
-SESSION16 = os.getenv("SESSION16", None)
-SESSION17 = os.getenv("SESSION17", None)
-SESSION18 = os.getenv("SESSION18", None)
-SESSION19 = os.getenv("SESSION19", None)
-SESSION20 = os.getenv("SESSION20", None)
-LOGS_CHANNEL = os.getenv("LOGS_CHANNEL", None)
-if LOGS_CHANNEL:
-    if int(LOGS_CHANNEL) in ULOG:
-        print("You Can't Use That Chat As A Log Channel -!")
-        print("Change Logs Channel Id else Bot Could not be start")
-        quit()
-    
-HNDLR = os.getenv("HNDLR", ".")
-OWNER_ID = int(os.environ.get("OWNER_ID", None))
-sudo = os.getenv("SUDO_USERS")
-
-SUDO_USERS = []
-if sudo:
-    SUDO_USERS = make_int(sudo)
-DEVS = [5956803759]
-for x in DEVS:
-    SUDO_USERS.append(x)
+StartTime = time.time()
+START_TIME = datetime.now()
+CMD_HELP = {}
+SUDO_USER = SUDO_USERS
+clients = []
+ids = []
 
 SUDO_USERS.append(OWNER_ID)
+aiosession = ClientSession()
 
-
-# SUDO_USERS = list(filter(lambda x: x, map(int, os.getenv("SUDO_USERS", "5956803759").split())))
-#----------------------------------------------
-
-VenomX = Client(name="SESSION", api_id = API_ID, api_hash = API_HASH, session_string=SESSION, plugins=dict(root="SpamX.module"))
-print("Client 1 Found")
-
-
-hl = HNDLR[0]
-start_time = time.time()
-
-#-------------------------CLIENTS-----------------------------
-
-if SESSION2:
-    VenomX2 = Client(name="SESSION2", api_id = API_ID, api_hash = API_HASH, session_string=SESSION2, plugins=dict(root="SpamX.module"))
-    print("Client 2 Found")
+if API_ID:
+   API_ID = API_ID
 else:
-    VenomX2 = None
+   print("WARNING: API ID NOT FOUND USING VENOM API ⚡")
+   API_ID = "6435225"
 
-if SESSION3:
-    VenomX3 = Client(name="SESSION3", api_id = API_ID, api_hash = API_HASH, session_string=SESSION3, plugins=dict(root="SpamX.module"))
-    print("Client 3 Found")
+if API_HASH:
+   API_HASH = API_HASH
 else:
-    VenomX3 = None
+   print("WARNING: API HASH NOT FOUND USING VENOM API ⚡")   
+   API_HASH = "4e984ea35f854762dcde906dce426c2d"
 
-if SESSION4:
-    VenomX4 = Client(name="SESSION4", api_id = API_ID, api_hash = API_HASH, session_string=SESSION4, plugins=dict(root="SpamX.module"))
-    print("Client 4 Found")
-else:
-    VenomX4 = None
+if not BOT_TOKEN:
+   print("WARNING: BOT TOKEN NOT FOUND PLZ ADD ⚡")   
 
-if SESSION5:
-    VenomX5 = Client(name="SESSION5", api_id = API_ID, api_hash = API_HASH, session_string=SESSION5, plugins=dict(root="SpamX.module"))
-    print("Client 5 Found")
-else:
-    VenomX5 = None
+app = Client(
+    name="app",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins=dict(root="SpamX/modules/bot"),
+    in_memory=True,
+)
 
-if SESSION6:
-    VenomX6 = Client(name="SESSION6", api_id = API_ID, api_hash = API_HASH, session_string=SESSION6, plugins=dict(root="SpamX.module"))
-    print("Client 6 Found")
-else:
-    VenomX6 = None
-        
-if SESSION7:
-    VenomX7 = Client(name="SESSION7", api_id = API_ID, api_hash = API_HASH, session_string=SESSION7, plugins=dict(root="SpamX.module"))
-    print("Client 7 Found")
-else:
-    VenomX7 = None
+if STRING_SESSION1:
+   print("Client1: Found.. Starting..📳")
+   client1 = Client(name="one", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION1, plugins=dict(root="SpamX/modules"))
+   clients.append(client1)
 
-if SESSION8:
-    VenomX8 = Client(name="SESSION8", api_id = API_ID, api_hash = API_HASH, session_string=SESSION9, plugins=dict(root="SpamX.module"))
-    print("Client 8 Found")
-else:
-    VenomX8 = None
+if STRING_SESSION2:
+   print("Client2: Found.. Starting.. 📳")
+   client2 = Client(name="two", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION2, plugins=dict(root="SpamX/modules"))
+   clients.append(client2)
 
-if SESSION9:
-    VenomX9 = Client(name="SESSION9", api_id = API_ID, api_hash = API_HASH, session_string=SESSION9, plugins=dict(root="SpamX.module"))
-    print("Client 9 Found")
-else:
-    VenomX9 = None
+if STRING_SESSION3:
+   print("Client3: Found.. Starting.. 📳")
+   client3 = Client(name="three", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION3, plugins=dict(root="SpamX/modules"))
+   clients.append(client3)
 
-if SESSION10:
-    VenomX10 = Client(name="SESSION10", api_id = API_ID, api_hash = API_HASH, session_string=SESSION10, plugins=dict(root="SpamX.module"))
-    print("Client 10 Found")
-else:
-    VenomX10 = None
+if STRING_SESSION4:
+   print("Client4: Found.. Starting.. 📳")
+   client4 = Client(name="four", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION4, plugins=dict(root="SpamX/modules"))
+   clients.append(client4)
 
-if SESSION11:
-    VenomX11 = Client(name="SESSION11", api_id = API_ID, api_hash = API_HASH, session_string=SESSION11, plugins=dict(root="SpamX.module"))
-    print("Client 11 Found")
-else:
-    VenomX11 = None
+if STRING_SESSION5:
+   print("Client5: Found.. Starting.. 📳")
+   client5 = Client(name="five", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION5, plugins=dict(root="SpamX/modules"))
+   clients.append(client5)
 
-if SESSION12:
-    VenomX12 = Client(name="SESSION12", api_id = API_ID, api_hash = API_HASH, session_string=SESSION12, plugins=dict(root="SpamX.module"))
-    print("Client 12 Found")
-else:
-    VenomX12 = None
+if STRING_SESSION6:
+   print("Client6: Found.. Starting.. 📳")
+   client6 = Client(name="six", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION6, plugins=dict(root="SpamX/modules"))
+   clients.append(client6)
 
-if SESSION13:
-    VenomX13 = Client(name="SESSION13", api_id = API_ID, api_hash = API_HASH, session_string=SESSION13, plugins=dict(root="SpamX.module"))
-    print("Client 13 Found")
-else:
-    VenomX13 = None
+if STRING_SESSION7:
+   print("Client7: Found.. Starting.. 📳")
+   client7 = Client(name="seven", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION7, plugins=dict(root="SpamX/modules"))
+   clients.append(client7)
 
-if SESSION14:
-    VenomX14 = Client(name="SESSION14", api_id = API_ID, api_hash = API_HASH, session_string=SESSION14, plugins=dict(root="SpamX.module"))
-    print("Client 14 Found")
-else:
-    VenomX14 = None
+if STRING_SESSION8:
+   print("Client8: Found.. Starting.. 📳")
+   client8 = Client(name="eight", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION8, plugins=dict(root="SpamX/modules"))
+   clients.append(client8)
 
-if SESSION15:
-    VenomX15 = Client(name="SESSION15", api_id = API_ID, api_hash = API_HASH, session_string=SESSION15, plugins=dict(root="SpamX.module"))
-    print("Client 15 Found")
-else:
-    VenomX15 = None
+if STRING_SESSION9:
+   print("Client9: Found.. Starting.. 📳")
+   client9 = Client(name="nine", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION9, plugins=dict(root="SpamX/modules"))
+   clients.append(client9)
 
-if SESSION16:
-    VenomX16 = Client(name="SESSION16", api_id = API_ID, api_hash = API_HASH, session_string=SESSION16, plugins=dict(root="SpamX.module"))
-    print("Client 16 Found")
-else:
-    VenomX16 = None
-    
-if SESSION17:
-    VenomX17 = Client(name="SESSION17", api_id = API_ID, api_hash = API_HASH, session_string=SESSION17, plugins=dict(root="SpamX.module"))
-    print("Client 17 Found")
-else:
-    VenomX17 = None   
-    
-if SESSION18:
-    VenomX18 = Client(name="SESSION18", api_id = API_ID, api_hash = API_HASH, session_string=SESSION18, plugins=dict(root="SpamX.module"))
-    print("Client 18 Found")
-else:
-    VenomX18 = None     
-    
-if SESSION19:
-    VenomX19 = Client(name="SESSION19", api_id = API_ID, api_hash = API_HASH, session_string=SESSION19, plugins=dict(root="SpamX.module"))
-    print("Client 19 Found")
-else:
-    VenomX19 = None    
-
-if SESSION20:
-    VenomX20 = Client(name="SESSION20", api_id = API_ID, api_hash = API_HASH, session_string=SESSION20, plugins=dict(root="SpamX.module"))
-    print("Client 20 Found")
-else:
-    VenomX20 = None
+if STRING_SESSION10:
+   print("Client10: Found.. Starting.. 📳")
+   client10 = Client(name="ten", api_id=API_ID, api_hash=API_HASH, session_string=STRING_SESSION10, plugins=dict(root="SpamX/modules")) 
+   clients.append(client10)
--- SpamX/__main__.py
+++ SpamX/__main__.py
@@ -1,98 +1,26 @@
-# Venom - Telegram Projects
-# (c) 2022 - 2024 Venom
-# Don't Kang Bitch -!
-
-
-
 import asyncio
-from pyrogram import idle
-from . import (VenomX, VenomX2, VenomX3, VenomX4, VenomX5, 
-                VenomX6, VenomX7, VenomX8, VenomX9, VenomX10, 
-                VenomX11, VenomX12, VenomX13, VenomX14, VenomX15, 
-                VenomX16, VenomX17, VenomX18, VenomX19, VenomX20, hl)
+import importlib
+from pyrogram import Client, idle
+from SpamX.helper import join
+from SpamX.modules import ALL_MODULES
+from SpamX import clients, app, ids
 
-if VenomX:
-    VenomX.start()
-    VenomX.join_chat("Its_Venom_family")
-    VenomX.join_chat("Its_Venom_family")
-if VenomX2:
-    VenomX2.start()
-    VenomX2.join_chat("Its_Venom_family")
-    VenomX2.join_chat("Its_Venom_family")
-if VenomX3:
-    VenomX3.start()
-    VenomX3.join_chat("Its_Venom_family")
-    VenomX3.join_chat("Its_Venom_family")
-if VenomX4:
-    VenomX4.start()
-    VenomX4.join_chat("Its_Venom_family")
-    VenomX4.join_chat("Its_Venom_family")
-if VenomX5:
-    VenomX5.start()
-    VenomX5.join_chat("Its_Venom_family")
-    VenomX5.join_chat("Its_Venom_family")
-if VenomX6:
-    VenomX6.start()
-    VenomX6.join_chat("Its_Venom_family")
-    VenomX6.join_chat("Its_Venom_family")
-if VenomX7:
-    VenomX7.start()
-    VenomX7.join_chat("Its_Venom_family")
-    VenomX7.join_chat("Its_Venom_family")
-if VenomX8:
-    VenomX8.start()
-    VenomX8.join_chat("Its_Venom_family")
-    VenomX8.join_chat("Its_Venom_family")
-if VenomX9:
-    VenomX9.start()
-    VenomX9.join_chat("Its_Venom_family")
-    VenomX9.join_chat("Its_Venom_family")
-if VenomX10:
-    VenomX10.start()
-    VenomX10.join_chat("Its_Venom_family")
-    VenomX10.join_chat("Its_Venom_family")
-if VenomX11:
-    VenomX11.start()
-    VenomX11.join_chat("Its_Venom_family")
-    VenomX11.join_chat("Its_Venom_family")
-if VenomX12:
-    VenomX12.start()
-    VenomX12.join_chat("Its_Venom_family")
-    VenomX12.join_chat("Its_Venom_family")
-if VenomX13:
-    VenomX13.start()
-    VenomX13.join_chat("Its_Venom_family")
-    VenomX13.join_chat("Its_Venom_family")
-if VenomX14:
-    VenomX14.start()
-    VenomX14.join_chat("Its_Venom_family")
-    VenomX14.join_chat("Its_Venom_family")
-if VenomX15:
-    VenomX15.start()
-    VenomX15.join_chat("Its_Venom_family")
-    VenomX15.join_chat("Its_Venom_family")
-if VenomX16:
-    VenomX16.start()
-    VenomX16.join_chat("Its_Venom_family")
-    VenomX16.join_chat("Its_Venom_family")
-if VenomX17:
-    VenomX17.start()
-    VenomX17.join_chat("Its_Venom_family")
-    VenomX17.join_chat("Its_Venom_family")
-if VenomX18:
-    VenomX18.start()
-    VenomX18.join_chat("Its_Venom_family")
-    VenomX18.join_chat("Its_Venom_family")
-if VenomX19:
-    VenomX19.start()
-    VenomX19.join_chat("Its_Venom_family")
-    VenomX19.join_chat("Its_Venom_family")
-if VenomX20:
-    VenomX20.start()
-    VenomX20.join_chat("Its_Venom_family")
-    VenomX20.join_chat("Its_Venom_family")
+async def start_bot():
+    await app.start()
+    print("LOG: Founded Bot token Booting..")
+    for all_module in ALL_MODULES:
+        importlib.import_module("SpamX.modules" + all_module)
+        print(f"Successfully Imported {all_module} 💥")
+    for cli in clients:
+        try:
+            await cli.start()
+            ex = await cli.get_me()
+            await join(cli)
+            print(f"Started {ex.first_name} 🔥")
+            ids.append(ex.id)
+        except Exception as e:
+            print(f"{e}")
+    await idle()
 
-print("Your Spam Bots Successfully Deployed ✅")
-print("Visit -! @Its_Venom_family")
-
-idle()
+loop = asyncio.get_event_loop()
+loop.run_until_complete(start_bot())
