db8b70a22e4ced545326e15685b5ba4e3beaba8f
taitangamer <129022006+taitangamer@users.noreply.github.com>
2023-04-12 06:39:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,19 +6,19 @@ if os.path.exists("local.env"):
     load_dotenv("local.env")
 
 
-API_ID = int(getenv("API_ID", "6435225")) #optional
-API_HASH = getenv("API_HASH", "") #optional
+API_ID = int(getenv("API_ID", "21840776")) #optional
+API_HASH = getenv("API_HASH", "a83dc6bea8b64c2191b2fe631f1097a9") #optional
 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1362133845").split()))
 OWNER_ID = int(getenv("OWNER_ID"))
 MONGO_URL = getenv("MONGO_URL")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-ALIVE_PIC = getenv("ALIVE_PIC", 'https://telegra.ph/file/17ded061a4ae0833a77b7.jpg')
+BOT_TOKEN = getenv("BOT_TOKEN", "5719989001:AAHkb_pp8Phz1Z8BgbZdp5hXUbWP7bfhqH4")
+ALIVE_PIC = getenv("ALIVE_PIC", 'https://telegra.ph/file/3b9730e0042579e85c243.jpg')
 ALIVE_TEXT = getenv("ALIVE_TEXT")
 PM_LOGGER = getenv("PM_LOGGER")
 LOG_GROUP = getenv("LOG_GROUP")
 GIT_TOKEN = getenv("GIT_TOKEN") #personal access token
-REPO_URL = getenv("REPO_URL", "https://github.com/Itzvenomo/SpamX")
+REPO_URL = getenv("REPO_URL", "https://github.com/taitangamer/SpamX")
 BRANCH = getenv("BRANCH", "main") #don't change
  
 STRING_SESSION1 = getenv("STRING_SESSION1", "")
